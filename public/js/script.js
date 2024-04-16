f1fbf94261687236e71edae62902241181d59b93
ycheryl <yulcheryl2357@gmail.com>
2024-04-16 23:34:32 +0700

    search effects

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,19 @@
-alert('Hello');
+document.addEventListener('DOMContentLoaded', () => {
+    const searchBtn = document.getElementById('searchBtn');
+    const searchBar = document.querySelector('.searchBar');
+    const searchInput = document.getElementById('searchInput');
+    const searchClose = document.getElementById('searchClose');
+
+    searchBtn.addEventListener('click', () => {
+        searchBar.style.visibility = "visible";
+        searchBar.classList.add("open");
+        this.setAttribute("aria-expanded", "true");
+        searchInput.focus();
+    })
+
+    searchClose.addEventListener('click', () => {
+        searchBar.style.visibility = "hidden";
+        searchBar.classList.remove("open");
+        this.setAttribute("aria-expanded", "false");
+    })
+})
