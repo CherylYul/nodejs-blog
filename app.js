1b14a6d775024aece61470d94294809baaba71e0
ycheryl <yulcheryl2357@gmail.com>
2024-04-18 16:09:09 +0700

    add cookie, update login and register

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+const session = require("express-session");
+const cookieParser = require('cookie-parser');
 const connectDB = require("./server/config/db");
+const MongoStore = require("connect-mongo");
 const app = express();
 const PORT = 5005 || process.env.PORT;
 
 connectDB();
 
 // Access search term
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "Keyboard dog",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+    cookie: { maxAge: new Date(Date.now() + 3600000) }, // cookie expired date
+  })
+);
 
-app.use(express.static('public')); //public folder
+app.use(express.static("public")); //public folder
 
 // Templating Engine
 app.use(expressLayout);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
-app.use('/', require('./server/routes/admin'));
-app.use('/', require('./server/routes/main'));
+app.use("/", require("./server/routes/admin"));
+app.use("/", require("./server/routes/main"));
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
+const bcrypt = require("bcrypt"); // encrypt and decrypt password and store it in database
+const jwt = require("jsonwebtoken");
 
 // Which layout we need to be run?
 const adminLayout = "../views/layouts/admin";
+const jwtSecret = process.env.JWT_SECRET;
 
 router.get("/admin", async (req, res) => {
   try {
@@ -18,17 +21,94 @@ router.get("/admin", async (req, res) => {
   }
 });
 
+// LOGIN
 router.post("/admin", async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      if (username === "admin" && password === "password") {
-        res.send("Log in successfully!");
-      } else {
-        res.send("Wrong password!");
-      }
-    } catch (error) {
-      console.log(error);
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
     }
-  });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie('token', token, {httpOnly: true});
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/dashboard", (req, res) => {
+  try {
+    const locals = {
+      title: "Admin Page",
+      discription: "Studying Nodejs Express & MongoDB",
+    };
+    res.render("admin/dashboard", { locals, adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// ADD MIDDLEWARE TO HELP US LOGOUT
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      const user = await User.create({
+        username,
+        password: hashedPassword,
+      });
+      res.status(201).json({
+        message: "User created",
+        user,
+      });
+      // {"message":"User created",
+      //  "user":{
+      //     "username":"cheryl",
+      //     "password":"$2b$10$sBgDe34EHbHgf4S0x3CwoeNQ96hEZCr6spLO9iq2Ts8mMXl8Q513.",
+      //     "_id":"6620da20f288a01b5a663b33",
+      //     "__v":0
+      // }}
+    } catch (error) {
+      if ((error.code = 11000)) {
+        res.status(409).json({
+          message: "User already in use",
+        });
+      }
+      res.status(500).json({
+        message: "Internal server error",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// FIRST CHECKING ADMIN
+// router.post("/admin", async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+//     if (username === "admin" && password === "password") {
+//       res.send("Log in successfully!");
+//     } else {
+//       res.send("Wrong password!");
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 module.exports = router;
